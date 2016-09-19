0dae38c4e5f81554dc62c9af7202c2ea215face7
Avinash Jaiswal <avinashj@microsoft.com>
2016-09-19 11:33:38 -0700

    v4

--- DatabaseScanner/Program.cs
+++ DatabaseScanner/Program.cs
@@ -111,7 +111,7 @@ namespace DatabaseScanner
             List<string> list = new List<string>();
             List<string> exclusionList = new List<string> { "master", "tempdb", "model", "msdb"};
 
-            // Open connection to the database Yo!
+            // Open connection to the database Yo!5
             ConnectionString cnStr = new ConnectionString();
             cnStr.server = server;
             using (SqlConnection sqlConnection = connectToDb(cnStr))
